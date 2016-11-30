0fb7152d86ac58d1177ebca4bbd0b98126e4e270
Hamilton Clark <dhclark2@ncsu.edu>
2016-11-30 18:28:00 -0500

    Adventure Mode done

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 require 'pg'
 
 get '/' do
+  database = PG.connect(dbname: "tiy-sports")
+
+  # Lets get all the teams from the database
+  @rows = database.exec("SELECT name, description, image_url, count(player_id) AS new_column FROM teams, memberships GROUP BY name, description, image_url")
 
   erb :home
 end
